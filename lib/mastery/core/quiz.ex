f22861c2f27b0f908e08c5d83d28a934e9d52fc5
psagan <patryk.sagan@icloud.com>
2021-04-14 17:22:29 +0200

    Add template function to quiz

--- lib/mastery/core/quiz.ex
+++ lib/mastery/core/quiz.ex
@@ -13,4 +13,20 @@ defmodule Mastery.Core.Quiz do
   def new(fields) do
     struct!(__MODULE__, fields)
   end
+
+  def add_template(quiz, fields) do
+    template = Template.new(fields)
+
+    templates =
+      update_in(
+        quiz.templates,
+        [template.category],
+        &add_to_list_or_nil(&1, template)
+      )
+
+    %{quiz | templates: templates}
+  end
+
+  defp add_to_list_or_nil(nil, template), do: [template]
+  defp add_to_list_or_nil(templates, template), do: [template | templates]
 end
