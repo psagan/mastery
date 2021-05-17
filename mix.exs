6e64cc206ccd54037f85c09da1581a060b52e99e
psagan <patryk.sagan@icloud.com>
2021-05-17 13:41:39 +0200

    Add some tests

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Mastery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:eex_html, "~> 1.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
