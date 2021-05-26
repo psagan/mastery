91f118709821637cbcc8d28dd1d47ed02ba48a1b
psagan <patryk.sagan@icloud.com>
2021-05-26 16:31:30 +0200

    Update formatting

--- lib/mastery/boundary/quiz_session.ex
+++ lib/mastery/boundary/quiz_session.ex
@@ -22,11 +22,12 @@ defmodule Mastery.Boundary.QuizSession do
   def handle_call({:answer_question, answer}, _from, {quiz, email}) do
     quiz
     |> Quiz.answer_question(Response.new(quiz, email, answer))
-    |> Quiz.select_question
+    |> Quiz.select_question()
     |> maybe_finish(email)
   end
 
   defp maybe_finish(nil, _email), do: {:stop, :normal, :finished, nil}
+
   defp maybe_finish(quiz, email) do
     {
       :reply,
