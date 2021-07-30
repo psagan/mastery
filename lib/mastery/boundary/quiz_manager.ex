7b3d753ecebb3c4bea402e01aef5aa0f3f8fb4dc
psagan <patryk.sagan@icloud.com>
2021-07-30 11:42:55 +0200

    Update mastery

--- lib/mastery/boundary/quiz_manager.ex
+++ lib/mastery/boundary/quiz_manager.ex
@@ -8,8 +8,8 @@ defmodule Mastery.Boundary.QuizManager do
 
   def init(_quizzes), do: {:error, "quizzes must be a map"}
 
-  def start_link(options \\ []) do
-    GenServer.start_link(__MODULE__, %{}, options)
+  def start_link(options \\ [ ]) do
+    GenServer.start_link(__MODULE__, %{ }, options)
   end
 
   def build_quiz(manager \\ __MODULE__, quiz_fields) do
@@ -24,9 +24,14 @@ defmodule Mastery.Boundary.QuizManager do
     GenServer.call(manager, {:lookup_quiz_by_title, quiz_title})
   end
 
+  def remove_quiz(manager \\ __MODULE__, quiz_title) do
+    GenServer.call(manager, {:remove_quiz, quiz_title})
+  end
+
   def handle_call({:build_quiz, quiz_fields}, _from, quizzes) do
     quiz = Quiz.new(quiz_fields)
     new_quizzes = Map.put(quizzes, quiz.title, quiz)
+
     {:reply, :ok, new_quizzes}
   end
 
@@ -35,15 +40,20 @@ defmodule Mastery.Boundary.QuizManager do
         _from,
         quizzes
       ) do
-    new_quizzes =
-      Map.update!(quizzes, quiz_title, fn quiz ->
-        Quiz.add_template(quiz, template_fields)
-      end)
+    new_quizzes = Map.update!(quizzes, quiz_title, fn quiz ->
+      Quiz.add_template(quiz, template_fields)
+    end)
 
     {:reply, :ok, new_quizzes}
   end
 
+  def handle_call({:remove_quiz, quiz_title}, _from, quizzes) do
+    new_quizzes = Map.delete(quizzes, quiz_title)
+    {:reply, :ok, new_quizzes}
+  end
+
   def handle_call({:lookup_quiz_by_title, quiz_title}, _from, quizzes) do
+
     {:reply, quizzes[quiz_title], quizzes}
   end
 end
--- lib/mastery/boundary/quiz_session.ex
+++ lib/mastery/boundary/quiz_session.ex
@@ -5,7 +5,7 @@ defmodule Mastery.Boundary.QuizSession do
   def child_spec({quiz, email}) do
     %{
       id: {__MODULE__, {quiz.title, email}},
-      start: {__MODULE__, :start_link, [{quiz, email}]},
+      start: { __MODULE__, :start_link, [{quiz, email}]},
       restart: :temporary
     }
   end
@@ -14,8 +14,7 @@ defmodule Mastery.Boundary.QuizSession do
     GenServer.start_link(
       __MODULE__,
       {quiz, email},
-      name: via({quiz.title, email})
-    )
+      name: via({quiz.title, email}))
   end
 
   def take_quiz(quiz, email) do
@@ -29,8 +28,34 @@ defmodule Mastery.Boundary.QuizSession do
     GenServer.call(via(name), :select_question)
   end
 
+
   def answer_question(name, answer) do
     GenServer.call(via(name), {:answer_question, answer})
+  end
+
+  def active_sessions_for(quiz_title) do
+    Mastery.Supervisor.QuizSession
+    |> DynamicSupervisor.which_children
+    |> Enum.filter(&child_pid?/1)
+    |> Enum.flat_map(&active_sessions(&1, quiz_title))
+  end
+
+  defp child_pid?({:undefined, pid, :worker, [__MODULE__]})
+       when is_pid(pid) do
+    true
+  end
+  defp child_pid?(_child), do: false
+
+  defp active_sessions({:undefined, pid, :worker, [__MODULE__]}, title) do
+    Mastery.Registry.QuizSession
+    |> Registry.keys(pid)
+    |> Enum.filter(fn {quiz_title, _email} ->
+      quiz_title == title
+    end)
+  end
+
+  def end_sessions(names) do
+    Enum.each(names, fn name -> GenServer.stop(via(name)) end)
   end
 
   def init({quiz, email}) do
@@ -39,18 +64,22 @@ defmodule Mastery.Boundary.QuizSession do
 
   def handle_call(:select_question, _from, {quiz, email}) do
     quiz = Quiz.select_question(quiz)
+
     {:reply, quiz.current_question.asked, {quiz, email}}
   end
 
-  def handle_call({:answer_question, answer}, _from, {quiz, email}) do
-    quiz
-    |> Quiz.answer_question(Response.new(quiz, email, answer))
-    |> Quiz.select_question()
+  def handle_call({:answer_question, answer, fun}, _from, {quiz, email}) do
+    fun = fun || fn r, f -> f.(r) end
+    response = Response.new(quiz, email, answer)
+    fun.(response, fn r ->
+      quiz
+      |> Quiz.answer_question(r)
+      |> Quiz.select_question
+    end)
     |> maybe_finish(email)
   end
 
   defp maybe_finish(nil, _email), do: {:stop, :normal, :finished, nil}
-
   defp maybe_finish(quiz, email) do
     {
       :reply,
@@ -59,7 +88,7 @@ defmodule Mastery.Boundary.QuizSession do
     }
   end
 
-  def via({_title, _email} = name) do
+  def via({_title, _email}=name) do
     {
       :via,
       Registry,
