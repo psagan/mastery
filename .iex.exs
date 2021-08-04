ec01ffae8b63ea9eb965b5e12f8f05148a8bc184
psagan <patryk.sagan@icloud.com>
2021-08-04 10:21:49 +0200

    Add subproject for persistence

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,3 @@
 alias Mastery.Core.{Template, Quiz, Response}
-alias Mastery.Boundary.{QuizManager, QuizSession}
+alias Mastery.Boundary.{QuizManager, QuizSession, Proctor}
 alias Mastery.Examples.{Math}
