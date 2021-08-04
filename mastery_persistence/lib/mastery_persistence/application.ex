7b9df079452fe79965673eff3c79b42015e309f9
psagan <patryk.sagan@icloud.com>
2021-08-04 10:27:34 +0200

    Ecto stuff

--- mastery_persistence/lib/mastery_persistence/application.ex
+++ mastery_persistence/lib/mastery_persistence/application.ex
@@ -8,6 +8,7 @@ defmodule MasteryPersistence.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      MasteryPersistence.Repo
       # Starts a worker by calling: MasteryPersistence.Worker.start_link(arg)
       # {MasteryPersistence.Worker, arg}
     ]
